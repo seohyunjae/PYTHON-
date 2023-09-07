af6700ed98f0eb862ec826bc496f4707aa2682b4
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-08 01:16:21 +0900

    연습

--- 5/4.py
+++ 5/4.py
@@ -1,6 +1,6 @@
 def recursive_function(i):
     # 100번째 호출을 했을 때 종료되도록 종료 조건 명시
-    if i == 100:
+    if i == 5:
         return
     print(i, '번째 재귀함수에서', i + 1, '번째 재귀함수를 호출합니다.')
     recursive_function(i + 1)
