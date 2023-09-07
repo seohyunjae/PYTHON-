d90b1af5eb81396895f77ae28b4e2d40a08fdcc0
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-07 18:14:11 +0900

    연습

--- 3/2.py
+++ 3/2.py
@@ -5,7 +5,8 @@ data = list(map(int, input().split()))
 
 data.sort() # 입력 받은 수들 정렬하기
 first = data[n - 1] # 가장 큰 수
-second = data[n - 2] # 두 번째로 큰 수
+second = data[n - 2] # 두 번째 가장 큰 수
+
 
 # 가장 큰 수가 더해지는 횟수 계산
 count = int(m / (k + 1)) * k
