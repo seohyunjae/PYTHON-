5f42b9ef072e1a6ec0dbd7dc9b20efb1f82798d7
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-10 18:29:26 +0900

    연습

--- 9/1.py
+++ 9/1.py
@@ -6,7 +6,6 @@ INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 n, m = map(int, input().split())
 # 시작 노드 번호를 입력받기
 start = int(input())
-# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
 graph = [[] for i in range(n + 1)]
 # 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
 visited = [False] * (n + 1)
