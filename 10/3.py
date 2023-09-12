ba135aee7688875ab755a738d01dd94b257e533c
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-12 23:45:54 +0900

    연습

--- 10/3.py
+++ 10/3.py
@@ -13,6 +13,7 @@ def union_parent(parent, a, b):
         parent[b] = a
     else:
         parent[a] = b
+        parent[a] = b
 
 # 노드의 개수와 간선(Union 연산)의 개수 입력 받기
 v, e = map(int, input().split())
