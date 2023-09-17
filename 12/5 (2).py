5224d9fef632c12982561ec0fe9832bca9d47073
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-18 01:52:15 +0900

    연습

--- 12/5 (2).py
+++ 12/5 (2).py
@@ -15,8 +15,18 @@ for _ in range(l):
     info.append((int(x), c))
     
 
-# 처음에는 오른쪽을 보고 있으므로(동,서,남,북)
-dx = []
+# 처음에는 오른쪽을 보고 있으므로(동,남,서,북)
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+def turn(direction, c):
+    if c == "L":
+        direction = (direction -1) % 4
+    else:
+        direction = (direction +1) % 4
+    return direction
+        
+
     
 
     
