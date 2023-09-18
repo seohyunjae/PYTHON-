4e70ad5645f134b87efcf0785c36f8edcf2d9278
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-18 21:37:18 +0900

    연습

--- 13/2 (2).py
+++ 13/2 (2).py
@@ -15,10 +15,56 @@ result = 0
 def virus(x, y):
     for i in range(4):
         nx = x + dx[i]
-        ny = x + dy[i]
+        ny = y + dy[i]
         # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
-        if nx >= 0 and nx < n and ny>=0 and ny < m:
+        if 0 <= nx < n and 0 <= ny < m:
             if temp[nx][ny] == 0:
                 # 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
                 temp[nx][ny] = 2
                 virus(nx, ny)
+                
+# 현재 맵에서 안전 영역의 크기를 계산하는 메서드
+def get_score():
+    score = 0 
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+# 깊이 우선 탐색(DFS)을 이용해 울타리를 설치하면서, 매번 안전 영역의 크기 계산
+def dfs(count):
+    global result
+    # 울타리가 3개 설치된 경우
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                 temp[i][j] = data[i][j]
+        # 각 바이러스의 위치에서 전파 진행
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i,j)
+        # 안전 영역의 최댓값 계산
+        result = max(result, get_score())
+        return
+    # 빈 공간에 울타리 설치
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                dfs(count)
+                data[i][j] = 0
+                count -= 1
+                
+dfs(0)
+print(result)
+
+
+
+
+    
+               
+
+                            
+            
