6c44a54ad8970ed8e115ae0f3fc9396bacb9a284
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-18 19:26:54 +0900

    연습

---
+++ 13/2 (2).py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+data = [] # 초기 맵 리스트
+temp = [[0] * m for _ in range(n)] # 벽을 설치한 뒤의 맵 리스트
+
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+    
+# 4가지 이동 방향에 대한 리스트
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+result = 0
+
+# 깊이 우선 탐색(DFS)을 이용해 각 바이러스가 사방으로 퍼지도록 하기
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = x + dy[i]
+        # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
+        if nx >= 0 and nx < n and ny>=0 and ny < m:
+            if temp[nx][ny] == 0:
+                # 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
+                temp[nx][ny] = 2
+                virus(nx, ny)
