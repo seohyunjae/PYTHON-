e1ea09cabb16f21ec2557c5321b53448e40275c4
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-20 00:31:33 +0900

    연습

--- 15/1.py
+++ 15/1.py
@@ -1,6 +1,6 @@
 from bisect import bisect_left, bisect_right
 
-# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수1
 def count_by_range(array, left_value, right_value):
     right_index = bisect_right(array, right_value)
     left_index = bisect_left(array, left_value)
