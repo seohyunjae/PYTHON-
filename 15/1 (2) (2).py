831da2646aa62b15f6dc799a85ceffd4d0715ba7
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-09-20 03:02:48 +0900

    연습

--- 15/1 (2) (2).py
+++ 15/1 (2) (2).py
@@ -1,17 +1,3 @@
-from bisect import bisect_left, bisect_right
-
-# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
-def count_by_range(a, left_value, right_value):
-    right_index = bisect_right(a, right_value)
-    left_index = bisect_left(a, left_value)
-    return right_index - left_index
-
-# 리스트 선언
-a = [1,2,3,3,3,3,4,4,8,9]
-
-# 값이 4인 데이터 개수 출력
-print(count_by_range(a,4,4))
-
-# 값이 [-1, 3] 범위에 있는 데이터 개수 출력
-print(count_by_range(a,-1,3))
-
+# 정렬된 수열에서 값이 x인 원소의 개수를 세는 메서드
+def count_by_value(array, x):
+    # 데이터의 개수
